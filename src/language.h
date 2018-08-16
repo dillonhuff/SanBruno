8337145659ccfdfa44f89b5b3cea4d2b6af39e16
Dillon Huff <dillonhuff@gmail.com>
2018-08-16 15:44:43 -0700

    Creating skeleton of a function

--- src/language.h
+++ src/language.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include "algorithm.h"
+
+namespace SanBruno {
+
+  class Function {
+  };
+
+  enum DataType {
+    DATA_TYPE_ARRAY,
+    DATA_TYPE_CUSTOM
+  };
+
+  class Data {
+  };
+
+  class Context {
+  public:
+
+    Data* arrayType(const int width) {
+      return nullptr;
+    }
+
+    Function* newFunction(const std::string& name,
+                          const std::map<std::string, Data*>& inputs,
+                          const std::map<std::string, Data*>& outputs) {
+      return nullptr;
+    }
+  };
+
+}
--- test/language_tests.cpp
+++ test/language_tests.cpp
@@ -1,6 +1,19 @@
 #include "catch.hpp"
 
+#include "language.h"
+
 namespace SanBruno {
 
-  
+  TEST_CASE("Express 32 bit floating point multiply") {
+    Context c;
+    Function* p = c.newFunction("float_add_32",
+                                {{"a", c.arrayType(32)},
+                                    {"b",c.arrayType(32)}},
+                                {{"result", c.arrayType(32)}});
+
+    
+
+    
+  }
+
 }
