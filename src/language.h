c43076070415d204593e665d309f2a27bd7a992f
Dillon Huff <dillonhuff@gmail.com>
2018-08-16 22:29:14 -0700

    How imperative or functional do I want this language to be?

--- src/language.h
+++ src/language.h
@@ -2,13 +2,79 @@
 
 #include "algorithm.h"
 
+using namespace dbhc;
+
 namespace SanBruno {
+
+  static inline
+  std::string tab(const int indentLevel) {
+    std::string ind = "";
+    for (int i = 0; i < indentLevel; i++) {
+      ind += "\t";
+    }
+    return ind;
+  }
 
   class Value {
   };
 
-  class Function {
+  enum StatementType {
+    STATEMENT_TYPE_FUNCTION_CALL,
+    STATEMENT_TYPE_CASE,
+    STATEMENT_TYPE_OPERATOR,
+    STATEMENT_TYPE_RETURN,
+  };
+
+  class Statement {
   public:
+
+    std::string toString(const int indentLevel) const {
+      return tab(indentLevel) + "STATEMENT";
+    }
+
+    virtual StatementType type() const = 0;
+
+    virtual ~Statement() {}
+  };
+
+  class Case : public Statement {
+  public:
+    virtual StatementType type() const {
+      return STATEMENT_TYPE_CASE;
+    }
+  };
+
+  
+  class Operator : public Statement {
+  public:
+    virtual StatementType type() const {
+      return STATEMENT_TYPE_OPERATOR;
+    }
+  };
+
+  class FunctionCall : public Statement {
+  public:
+    virtual StatementType type() const {
+      return STATEMENT_TYPE_FUNCTION_CALL;
+    }
+  };
+
+  class ReturnStmt : public Statement {
+  public:
+    virtual StatementType type() const {
+      return STATEMENT_TYPE_RETURN;
+    }
+  };
+  
+  class Function {
+    std::vector<Statement*> statements;
+
+  public:
+
+    std::vector<Statement*> getStatements() const {
+      return statements;
+    }
+
     Value* addFunctionCall(const std::string& str);
 
     Value* getValue(const std::string& name) {
@@ -16,19 +82,32 @@ namespace SanBruno {
     }
 
     Value* addSlice(const int end, const int start, Value* v) {
+      statements.push_back(new FunctionCall());
       return nullptr;
     }
 
     Value* addEquals(const Value* a, const Value* b) {
+      statements.push_back(new FunctionCall());
       return nullptr;
     }
 
     Value* addCase(const Value* res, const Value* ifTrue, const Value* ifFalse) {
+      statements.push_back(new Case());
       return nullptr;
     }
 
     Value* addConstant(const int bitWidth, const int Value) {
       return nullptr;
+    }
+
+    void setReturn(const std::string& returnName, const Value* value) {
+      statements.push_back(new ReturnStmt());
+    }
+
+    ~Function() {
+      for (auto stmt : statements) {
+        delete stmt;
+      }
     }
 
   };
@@ -56,6 +135,18 @@ namespace SanBruno {
       auto f = new Function();
       functions.insert({name, f});
       return f;
+    }
+
+    // Synthesize by unfolding the entire compute graph
+    void synthesizeNaive(const std::string& functionName) {
+      Function* f = map_find(functionName, functions);
+
+      for (auto stmt : f->getStatements()) {
+        std::cout << stmt->toString(0) << std::endl;
+      }
+
+      assert(false);
+
     }
 
     ~Context() {
--- test/language_tests.cpp
+++ test/language_tests.cpp
@@ -4,16 +4,16 @@
 
 namespace SanBruno {
 
-  TEST_CASE("Express 32 bit add") {
+  TEST_CASE("Express 32 bit add sign bit computation") {
     Context c;
-    Function* p = c.newFunction("float_add_32",
+    Function* p = c.newFunction("float_add_32_sign_bit",
                                 {{"a", c.arrayType(32)},
                                     {"b",c.arrayType(32)}},
-                                {{"result", c.arrayType(32)}});
+                                {{"sign_bit", c.arrayType(1)}});
 
     REQUIRE(p != nullptr);
 
-    // Now: Create function definition and test it
+    // Now: Create function definition
     Value* aSgnBit = p->addSlice(31, 31, p->getValue("a"));
     Value* bSgnBit = p->addSlice(31, 31, p->getValue("b"));
 
@@ -23,6 +23,15 @@ namespace SanBruno {
       p->addCase(res,
                  p->addConstant(1, 1),
                  p->addConstant(1, 0));
+
+    p->setReturn("sign_bit", resSignBit);
+
+    REQUIRE(p->getStatements().size() == 5);
+
+    // Synthesize this function
+    c.synthesizeNaive("float_add_32_sign_bit");
+
+    
   }
 
 }
